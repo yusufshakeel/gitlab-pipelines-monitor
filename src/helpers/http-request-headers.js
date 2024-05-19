cb2b6ee394704899a42b3885637d2c44de4a3bad
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2024-05-19 20:34:55 +0530

    request headers

---
+++ src/helpers/http-request-headers.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function getHeaders(data) {
+  const personalAccessToken = data.personalAccessToken.length
+    ? { 'PRIVATE-TOKEN': data.personalAccessToken }
+    : {};
+  return {
+    ...personalAccessToken
+  };
+}
+
+module.exports = { getHeaders };
