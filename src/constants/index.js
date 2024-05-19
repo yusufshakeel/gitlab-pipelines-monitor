dd3449de07e3fe7acb0eecf6917038d1b5ddcab6
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2024-05-19 20:32:15 +0530

    constants added

---
+++ src/constants/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const HOME_DIR = require('os').homedir();
+const GLPM_COMMAND = 'glpm';
+const GLPM_CONFIG_FILE = '.glpmrc';
+const GLPM_CONFIG_FILE_PATH = `${HOME_DIR}/${GLPM_CONFIG_FILE}`;
+const DEFAULT_API_TIMEOUT = process.env.DEFAULT_API_TIMEOUT || 5000;
+const PER_PAGE = process.env.PER_PAGE || 16;
+const CHECK_STATUS_EVERY_N_MILLISECONDS = process.env.CHECK_STATUS_EVERY_N_MILLISECONDS || 60000;
+const DEFAULT_API_ENDPOINT = process.env.DEFAULT_API_ENDPOINT || 'https://gitlab.com/api/v4';
+
+module.exports = {
+  HOME_DIR,
+  GLPM_COMMAND,
+  GLPM_CONFIG_FILE,
+  GLPM_CONFIG_FILE_PATH,
+  DEFAULT_API_TIMEOUT,
+  PER_PAGE,
+  CHECK_STATUS_EVERY_N_MILLISECONDS,
+  DEFAULT_API_ENDPOINT
+};
