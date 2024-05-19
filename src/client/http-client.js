837634dface00730cbfcb789e966abfd3108f7ad
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2024-05-19 20:32:50 +0530

    http-client added, using axios

---
+++ src/client/http-client.js
@@ -0,0 +1,56 @@
+'use strict';
+const axios = require('axios');
+
+module.exports = function HttpClient({ baseURL, timeout }) {
+  console.log('[HTTP Client] baseURL:', baseURL);
+  console.log('[HTTP Client] timeout:', timeout);
+
+  const instance = axios.create({ baseURL, timeout });
+
+  const setHeaders = headers =>
+    instance.interceptors.request.use(req => {
+      req.headers = { ...req.headers, headers, ...headers };
+      return req;
+    });
+
+  instance.interceptors.request.use(req => {
+    console.log(`[HTTP Client] Request: ${req.method.toUpperCase()} ${req.baseURL}${req.url}`);
+    return req;
+  });
+
+  instance.interceptors.response.use(
+    res => {
+      console.log(
+        `[HTTP Client] Response: ${res.status} ${res.config.method.toUpperCase()} ${
+          res.config.baseURL
+        }${res.config.url}`
+      );
+      if (res.status === 200) {
+        return { status: res.status, data: res.data };
+      }
+      return res;
+    },
+    err => {
+      console.log(`[HTTP Client] Response error: ${err}`);
+    }
+  );
+
+  const timeoutSetup = source =>
+    setTimeout(() => source.cancel(`[HTTP Client] Request timed out after ${timeout}ms.`), timeout);
+
+  const get = async ({ url, headers }) => {
+    Object.keys(headers).length && setHeaders(headers);
+    const source = axios.CancelToken.source();
+    const timeoutId = timeoutSetup(source);
+    try {
+      return await instance.get(url, { cancelToken: source.token });
+    } catch (e) {
+      console.error(`[HTTP Client] Error GET ${url}: ${e.message}`);
+      throw e;
+    } finally {
+      clearTimeout(timeoutId);
+    }
+  };
+
+  return { get };
+};
