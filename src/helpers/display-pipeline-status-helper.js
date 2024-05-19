3727107ae609676e23b27cc3a39f87e070de4b98
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2024-05-19 23:14:09 +0530

    added colors

--- src/helpers/display-pipeline-status-helper.js
+++ src/helpers/display-pipeline-status-helper.js
@@ -1,13 +1,9 @@
 'use strict';
+const colors = require('colors');
 
 const getStatus = d => {
   const columnWidth = 22;
-  let status = '';
-  if (d.status === 'success') {status = '✅ PASSED';}
-  else if (d.status === 'failed') {status =  '❌ FAILED';}
-  else {
-    status = d.status.toUpperCase();
-  }
+  let status = d.status.toUpperCase();
   const rightPadding = new Array(columnWidth - status.length).fill(' ').join('');
   return `${status}${rightPadding}`;
 };
@@ -31,22 +27,38 @@ Default Branch: ${project.defaultBranch}
 Project Url: ${project.projectUrl}`
   );
   console.log(
-    '+--------------------------+-------------------------+-----------------+-----------+---------------'
+    '+--------------------------+------------------------+-----------------+-----------+---------------'
   );
-  console.log('| Timestamp                | Status                  | Pipeline        | Commit    | Branch');
   console.log(
-    '+--------------------------+-------------------------+-----------------+-----------+---------------'
+    '| Timestamp                | Status                 | Pipeline        | Commit    | Branch'
+  );
+  console.log(
+    '+--------------------------+------------------------+-----------------+-----------+---------------'
   );
   pipelines.forEach(d => {
     const updatedAt = d['updated_at'];
-    console.log(
-      `| ${updatedAt} | ${getStatus(d)} | ${getPipelineId(d)} | ${getShortCommitId(
-        d
-      )} | ${getBranchName(d)}`
-    );
+    if (d.status === 'success') {
+      console.log(
+        `| ${updatedAt} | ${colors.green(getStatus(d))} | ${getPipelineId(d)} | ${getShortCommitId(
+          d
+        )} | ${getBranchName(d)}`
+      );
+    } else if (d.status === 'failed') {
+      console.log(
+        `| ${updatedAt} | ${colors.bgRed(getStatus(d))} | ${getPipelineId(d)} | ${getShortCommitId(
+          d
+        )} | ${getBranchName(d)}`
+      );
+    } else {
+      console.log(
+        `| ${updatedAt} | ${getStatus(d)} | ${getPipelineId(d)} | ${getShortCommitId(
+          d
+        )} | ${getBranchName(d)}`
+      );
+    }
   });
   console.log(
-    '+--------------------------+-------------------------+-----------------+-----------+---------------'
+    '+--------------------------+------------------------+-----------------+-----------+---------------'
   );
 }
 
