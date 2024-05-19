83ad6b13f5144f14033169819f6c29f72dfdaead
Yusuf Shakeel <6522504+yusufshakeel@users.noreply.github.com>
2024-05-19 20:36:29 +0530

    init command handler

---
+++ src/commands/initialise.js
@@ -0,0 +1,46 @@
+'use strict';
+const fs = require('fs');
+const readline = require('readline');
+const {
+  GLPM_CONFIG_FILE,
+  GLPM_CONFIG_FILE_PATH,
+  DEFAULT_API_TIMEOUT,
+  PER_PAGE,
+  DEFAULT_API_ENDPOINT
+} = require('../constants');
+const getProjectInput = require('../helpers/get-project-input');
+
+const getConfig = project => {
+  return {
+    projects: {
+      [project.projectId]: project,
+      ['default']: project.projectId
+    },
+    api: {
+      defaultApiEndpoint: DEFAULT_API_ENDPOINT,
+      timeout: DEFAULT_API_TIMEOUT,
+      perPage: PER_PAGE
+    }
+  };
+};
+
+async function run() {
+  const readLine = readline.createInterface({ input: process.stdin, output: process.stdout });
+  try {
+    const prompt = query => new Promise(resolve => readLine.question(query, resolve));
+
+    console.log(`This command will help you in creating the ${GLPM_CONFIG_FILE} file.`);
+    console.log('Press ^C at any time to quit.\n');
+
+    const input = await getProjectInput(prompt);
+
+    fs.writeFileSync(GLPM_CONFIG_FILE_PATH, JSON.stringify(getConfig(input)), 'utf8');
+    console.log('Done!');
+  } catch (error) {
+    console.error('ERROR. init:', error.message);
+  } finally {
+    readLine.close();
+  }
+}
+
+module.exports = run;
